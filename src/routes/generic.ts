ed4920ce11fb300cad84b27ae43d81772f55fff4
obrin <jordanyong23@gmail.com>
2022-12-31 17:45:38 -0500

    debug

--- src/routes/generic.ts
+++ src/routes/generic.ts
@@ -2,7 +2,7 @@ import * as express from 'express'
 
 const createRouter = (router: express.Router) => {
   router.get('/health', (_: express.Request, res: express.Response) => {
-    return res.send('OK V3')
+    return res.send('OK V4')
   })
 
   return router
